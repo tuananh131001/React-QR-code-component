c3d1e17b2efd77609b925c7ea6d192cbb54fffdd
w <s3864077@rmit.edu.vn>
2022-03-22 18:14:43 +0700

    sde

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import image from "./images/image-qr-code.png"
+import './App.css'
+const cardComponent = {
+  img: image,
+  title: "Improve your front-end skills by building projects",
+  description:
+    "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level",
+};
+
+const Card = ({ img , title , description }) => {
+  return (
+    <div className="card">
+      <img src={img} alt="" className="image" />
+      <h1 className="title">{title}</h1>
+      <h2 className="description">{description}</h2>
+    </div>
+  );
+};
+
+function App() {
+  const title = "Cho Quan";
+  return (
+    <div className="app">
+      <section>
+        <Card
+          img={cardComponent.img}
+          title={cardComponent.title}
+          description={cardComponent.description}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default App;
