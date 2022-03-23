47a911b17dbc308a3c1574dc6702fcea6de21c37
William <s3864077@rmit.edu.vn>
2022-03-23 14:07:18 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import image from "./images/image-qr-code.png"
-import './App.css'
+import image from "./images/image-qr-code.png";
+import "./App.css";
 const cardComponent = {
   img: image,
   title: "Improve your front-end skills by building projects",
@@ -7,12 +7,14 @@ const cardComponent = {
     "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level",
 };
 
-const Card = ({ img , title , description }) => {
+const Card = ({ img, title, description }) => {
   return (
     <div className="card">
       <img src={img} alt="" className="image" />
-      <h1 className="title">{title}</h1>
-      <h2 className="description">{description}</h2>
+      <div className="card-text">
+        <h1 className="title">{title}</h1>
+        <h2 className="description">{description}</h2>
+      </div>
     </div>
   );
 };
